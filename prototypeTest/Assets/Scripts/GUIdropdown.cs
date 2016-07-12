cd944d19b323ed25c8a2bde57927bab8eb753fe5
osvil <osvil.fbx@gmail.com>
2016-07-11 20:53:13 -0500

    prototype upload
    does this work?

---
+++ prototypeTest/Assets/Scripts/GUIdropdown.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIdropdown : MonoBehaviour
+{
+
+    //NOT USING THIS SCRIPT ATM
+
+    public Dropdown sectdropdown;
+    public int sectionNum = 0;
+    //public Navigation nav;
+
+    public GameObject[] sections;
+
+    // Use this for initialization
+    void Start()
+    {
+        //nav = GameObject.Find("Capsule").GetComponent<Navigation>();
+        sectdropdown.onValueChanged.AddListener(delegate
+        {
+            DropdownValueChanged(sectdropdown);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Destroy()
+    {
+        sectdropdown.onValueChanged.RemoveAllListeners();
+    }
+
+    private void DropdownValueChanged(Dropdown target)
+    {
+        print("Selected" + target.value);
+
+        sectionNum = 0;
+
+        sectionNum = target.value - 1;
+
+    }
+
+    public void SetDropdownIndex(int index)
+    {
+        sectdropdown.value = index;
+    }
+
+}
---
+++ prototypeTest/Assets/Scripts/Navigation.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Navigation : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Dropdown dropdown;
+    //public GUIdropdown sectDrop;
+
+    //section A (blue)
+    public Transform[] sectionBlue;
+
+    //section B (red)
+    public Transform[] sectionRed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //reference to nav mesh
+        agent = GetComponent<NavMeshAgent>();
+
+        //dropdown delagate (event)
+        dropdown.onValueChanged.AddListener(delegate
+        {
+            DropdownValueChanged(dropdown);
+        });
+
+       // sectDrop = GameObject.Find("Level").GetComponent<GUIdropdown>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    //test
+
+
+    //do once
+    void Destroy()
+    {
+        dropdown.onValueChanged.RemoveAllListeners();
+    }
+
+    //add code to move here
+    private void DropdownValueChanged(Dropdown target)
+    {
+        int seat = 0;
+
+        print("Selected " + target.value);
+
+        seat = target.value - 1;
+
+        //sectionRed.Length
+
+        agent.SetDestination(sectionRed[seat].transform.position);
+    }
+
+    //keep track of dropdown values
+    public void SetDropdownIndex(int index)
+    {
+        dropdown.value = index;
+    }
+}
